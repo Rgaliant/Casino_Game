e766eed654b86080c00288431d4206a9194adfe5
Ryan Gale <ryanwfgale@gmail.com>
2019-02-15 14:56:19 -0700

    add player class

--- casino.rb
+++ casino.rb
@@ -10,20 +10,30 @@
 # Use classes to start the casino, bankroll, and each individual game
 # Player places bet and wins / loses (hint: rand)
 # Player's bankroll goes up and down with wins and losses
-
-class CasinoGame
-  @player = []
-
-  def main_menu
-    puts "Welcome to The Trinity Casino"
+require 'pry'
+class Player
+  attr_accessor :name, :wallet
+def initialize
+  
+## player and wallet
+  puts "Welcome to The Trinity Casino"
     puts "What is your name?"
     name = gets.strip
     puts "Hello, #{name}, and how much money will you be playing with?"
     wallet = gets.strip
-      @name << name
-      @wallet << wallet
-    return { name: name, wallet: wallet }
+    binding.pry
+ end
+ #bonus method for player settings
+end
+
+
+
+class CasinoGame
+  player = Player.new
+  main_menu
+  def main_menu
+  
   end
 end
 
-CasinoGame.new
+CasinoGame
