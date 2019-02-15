567d759dcc3edffe88617fc54930cb096b057768
Ryan Gale <ryanwfgale@gmail.com>
2019-02-15 14:06:04 -0700

    intial commit

---
+++ casino.rb
@@ -0,0 +1,6 @@
+## class to handle game
+## methods for little games
+## class to handle person and wallet amount, user bankroll persists
+## Start game player has a name and initial bankroll
+#player can go 
+puts "Hello world"
