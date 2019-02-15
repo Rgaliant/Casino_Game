d5d1821ddf50f2090bec9a5ef4ec5a8a5d1a8402
Chris Larsen <c.r.larsen@hotmail.com>
2019-02-15 14:29:34 -0700

    add notes

--- casino.rb
+++ casino.rb
@@ -1,6 +1,29 @@
-## class to handle game
-## methods for little games
-## class to handle person and wallet amount, user bankroll persists
-## Start game player has a name and initial bankroll
-#player can go 
-puts "Hello world"
+# ## class to handle game
+# ## methods for little games
+# ## class to handle person and wallet amount, user bankroll persists
+# ## Start game player has a name and initial bankroll
+# #player can go 
+# puts "Hello world"
+
+# #class casino
+# #main program
+# #display menu
+# chose game
+
+
+# #class wallet
+# init amount
+# winning amount
+# losing amounts
+
+
+# #class user
+# get nameget a wallet amount
+
+
+# #class game 1
+# game1
+
+# #class game 2
+# game1
+
